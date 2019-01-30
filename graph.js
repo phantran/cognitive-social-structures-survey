527e62444a1867e0e7663d1fd8cd558e6ded1201
tphan <takeonme>
2019-01-30 22:38:34 +0100

    delete

--- graph.js
+++
@@ -1,9 +0,0 @@
-//--------------------------------
-// Declaration of graph properties
-//--------------------------------
-
-var svg = d3.select("body").append("svg")
-  .attr("width", bodyWidth)
-  .attr("height", bodyHeight)
-  .on("contextmenu", function() {d3.event.preventDefault()});
-
